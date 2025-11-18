fd93635071973a006ec39c76266deb2d785b24d4
robin_huebner@hotmail.com <robin_huebner@hotmail.com>
2025-11-18 10:26:23 +0000

    Changed bronze to silver

--- Infrastructure Setup/dab_data_engineering/resources/dab_data_engineering_etl/transformations/bronze_to_gold.sql
+++ Infrastructure Setup/dab_data_engineering/resources/dab_data_engineering_etl/transformations/bronze_to_gold.sql
@@ -1,5 +1,5 @@
--- Date dimension (CSV -> bronze streaming)
-CREATE OR REFRESH STREAMING TABLE bronze.date AS
+-- Date dimension (CSV -> silver streaming)
+CREATE OR REFRESH STREAMING TABLE silver.date AS
 SELECT
     *
 FROM STREAM read_files(
@@ -7,8 +7,8 @@ FROM STREAM read_files(
   format => 'csv'
 );
 
--- Store dimension (CSV -> bronze streaming)
-CREATE OR REFRESH STREAMING TABLE bronze.store AS
+-- Store dimension (CSV -> silver streaming)
+CREATE OR REFRESH STREAMING TABLE silver.store AS
 SELECT
     *
 FROM STREAM read_files(
@@ -16,8 +16,8 @@ FROM STREAM read_files(
   format => 'csv'
 );
 
--- Customer dimension (CSV -> bronze streaming)
-CREATE OR REFRESH STREAMING TABLE bronze.customer AS
+-- Customer dimension (CSV -> silver streaming)
+CREATE OR REFRESH STREAMING TABLE silver.customer AS
 SELECT
     *
 FROM STREAM read_files(
@@ -25,8 +25,8 @@ FROM STREAM read_files(
   format => 'csv'
 );
 
--- Product dimension (CSV -> bronze streaming)
-CREATE OR REFRESH STREAMING TABLE bronze.product AS
+-- Product dimension (CSV -> silver streaming)
+CREATE OR REFRESH STREAMING TABLE silver.product AS
 SELECT
     *
 FROM STREAM read_files(
@@ -34,8 +34,8 @@ FROM STREAM read_files(
   format => 'csv'
 );
 
--- Coffee sales fact (Parquet -> bronze streaming)
-CREATE OR REFRESH STREAMING TABLE bronze.coffee_sales AS
+-- Coffee sales fact (Parquet -> silver streaming)
+CREATE OR REFRESH STREAMING TABLE silver.coffee_sales AS
 SELECT
     *
 FROM STREAM read_files(
@@ -44,24 +44,24 @@ FROM STREAM read_files(
 );
 
 CREATE MATERIALIZED VIEW gold.dim_date AS
-SELECT * FROM bronze.date;
+SELECT * FROM silver.date;
 
 CREATE MATERIALIZED VIEW gold.dim_store AS
-SELECT * FROM bronze.store;
+SELECT * FROM silver.store;
 
 CREATE MATERIALIZED VIEW gold.dim_customer AS
-SELECT * FROM bronze.customer;
+SELECT * FROM silver.customer;
 
 CREATE MATERIALIZED VIEW gold.dim_product AS
-SELECT * FROM bronze.product;
+SELECT * FROM silver.product;
 
 CREATE MATERIALIZED VIEW gold.fact_coffee_sales AS
 SELECT
     fcs.*
     -- dp.list_price_usd * fcs.quantity_sold                               AS gross_revenue_usd,
     -- (dp.list_price_usd * fcs.quantity_sold) / (1 + ds.tax_rate)         AS net_revenue_usd
-FROM bronze.coffee_sales fcs
-JOIN bronze.product dp
+FROM silver.coffee_sales fcs
+JOIN silver.product dp
   ON fcs.product_key = dp.product_key
-JOIN bronze.store ds
+JOIN silver.store ds
   ON fcs.store_key = ds.store_key;
